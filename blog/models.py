2e836233e3ef5fd3b4e2f443c2eb74c98470ca75
justdoit <tangxushine@163.com>
2017-10-09 18:29:16 +0800

    template模板之filter

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,21 @@ class User(models.Model):
     username = models.CharField(max_length=64)
     email = models.CharField(max_length=64)
 
+    # role = models.ForeignKey("Role")  # 一对多,
+
+    role = models.ManyToManyField("Role")
+
+    def __str__(self):
+        return self.username
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=64)
+    code = models.CharField(max_length=64)
+    desc = models.TextField()
+
+    user = models.ManyToManyField("User")
+
 
 class Article(models.Model):
     name = models.CharField(max_length=128)
